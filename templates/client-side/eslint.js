f12c4be24b48c9bce03d1e67bb7574ba9d83c4e9
JoshD <joshuagardennis@gmail.com>
2020-03-07 15:55:17 +0000

    -

---
+++ templates/client-side/eslint.js
@@ -0,0 +1,14 @@
+module.exports = `
+{
+    "parser": "babel-eslint",
+    "extends": "react",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    }
+}`;
